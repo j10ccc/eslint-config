76509cef274e28eb54bd5feb6cb28a87fa3c3acf
j10c <blyb1739@gmail.com>
2024-01-18 18:37:19 +0800

    feat(astro): add astro project support

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,9 +1,11 @@
 const { isPackageExists } = require("local-pkg");
 
 const isReactUsed = isPackageExists("react");
+const isAstroUsed = isPackageExists("astro");
 
 module.exports = {
   extends: [
     isReactUsed && "@j10c/eslint-config-react",
+    isAstroUsed && "@j10c/eslint-config-astro",
   ].filter(Boolean)
 };
