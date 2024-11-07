9c1e94b21afde0fed613c1defb1bc7cd2d74078e
j10c <blyb1739@gmail.com>
2024-11-07 21:08:01 +0800

    fix(lint): remove unused options

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,9 +2,7 @@ import { Linter } from "eslint";
 import j10c from "./src";
 
 export default [
-  ...await j10c({
-    react: true
-  }),
+  ...await j10c(),
   {
     name: "local",
     ignores: [
